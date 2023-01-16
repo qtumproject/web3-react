d4da663188335b86e247c77ffe3f79ccbeb1ac56
snowwhitedev <snowwhite91924@gmail.com>
2023-01-16 16:29:34 +0700

    re:

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,4 @@
 export { createWeb3ReactRoot, Web3ReactProvider, useWeb3React, getWeb3ReactContext, PRIMARY_KEY } from './provider'
 export { UnsupportedChainIdError } from './manager'
+
+const abcd = 0;
